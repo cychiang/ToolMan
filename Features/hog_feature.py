a2e0c4d53b4853e1319a004b5ee427614e7f3a47
Casper CY Chiang <cychiang0823@gmail.com>
2015-04-08 15:36:13 +0800

    update: new tool: list_generator.py based on glob. It can help you to generate file list on specific path

--- Features/hog_feature.py
+++ Features/hog_feature.py
@@ -2,7 +2,7 @@
 import numpy as np
 import itertools as it
 from glob import glob
-import cv2, sys, os
+import cv2, sys, os, argparse
 
 help_message = '''
 USAGE: hog_feature.py <image_names> ...
@@ -36,34 +36,47 @@ def hog_feature(image, num_of_bin):
     print 'print_bins_info: bins'
     print_bins_info(bins)
 
-def hog_opencv_feature(image, winSize=(64,32), blockSize=(16,16), blockStride=(8,8), cellSize=(8,8), nbins=9):
-    # winSize, blockSize, blockStride, cellSize, nbins
-    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
-    r = [[0,0]]
-    descriptors = hog.compute(image,hog.blockStride,hog.cellSize,r)
-    print_size_info(image)
-    print 'model path: %s' %(os.path.dirname(cv2.__file__))
+def print_hog_info(hog):
     print 'hog winSize: %s' %(str(hog.winSize))
     print 'hog blockSize: %s' %(str(hog.blockSize))
     print 'hog blockStride: %s' %(str(hog.blockStride))
     print 'hog cell size: %s' %(str(hog.cellSize))
     print 'hog num of bins: %s' %(str(hog.nbins))
+
+
+def hog_opencv_feature(image, winSize=(64,128), blockSize=(16,16), blockStride=(8,8), cellSize=(8,8), nbins=9):
+    # winSize, blockSize, blockStride, cellSize, nbins
+    hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
+    r = [[0,0]]
+    descriptors = hog.compute(image,hog.blockStride,hog.cellSize,r)
+    print_size_info(image)
+    print_hog_info(hog)
     print 'size of descriptors: %d' %(len(descriptors))
+    return descriptors
 
 if __name__ == "__main__":
-    print 'version: %s' %(cv2.__version__)
+    # command line argument:
+    # Initial argument
+    parser = argparse.ArgumentParser(description='This program is generate the hog feature into file.')
+    parser.add_argument('-f', '--file', help='Input: The list of target images')
 
+    parser.add_argument('-o', '--out', help='Output: The collection of hog features')
+
+    # parser.add_argument('-', '--', help='')
+
+    print 'version: %s' %(cv2.__version__)
+    # Collect the feature from hog compute
+    descriptors = []
     for image_name in sys.argv[1:]:
         try:
-            image = cv2.imread(image_name)
+            image = cv2.imread(image_name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
             if image is None:
                 print 'Failed to load image file: %s' %(image)
                 continue
             else:
                 print 'loading ... %s' %(image_name)
-
-            # hog_feature(image, 8)
-            hog_opencv_feature(image)
+            # feature = hog_opencv_feature(image)
+            descriptors.append(hog_opencv_feature(image))
 
         except:
             print 'loading error'
